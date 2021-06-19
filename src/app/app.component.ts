1c8441f9c8b8df72595e84ecfb987afac519b4eb
Alex Dorogensky <alex@exebar.com>
2021-06-19 13:55:15 -0700

    Enable filtering and sorting and fetch data with an api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ag-grid-bootstrap';
   columns = [
-    { field: 'name' },
-    { field: 'dept' }
+    { field: 'make', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'price', sortable: true, filter: true }
   ];
-  rows = [{
-    name: 'Alex',
-    dept: 'Engineering',
-  }, {
-    name: 'Nicole',
-    dept: 'Marketing'
-  }, {
-    name: 'Max',
-    dept: 'Accounting'
-  }];
+  rows: Observable<any[]> | undefined;
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.rows = this.httpClient.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgGridModule } from "ag-grid-angular";
+import { HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AgGridModule } from "ag-grid-angular";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AgGridModule.withComponents([])
   ],
   providers: [],
