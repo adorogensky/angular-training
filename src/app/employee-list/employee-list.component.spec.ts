f22db3ae97dfe17077636e227a6cca854c676807
Alex Dorogensky <alex@exebar.com>
2021-09-09 18:54:00 -0700

    fix employee list comp test to show ag-grid-angular table, add test to verify table has a 'Name' column

--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,13 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeListComponent } from './employee-list.component';
+import { EmployeeService } from '../employee.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
+  const employeeService = new EmployeeService();
+  let getEmployeesSpy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports: [ AgGridModule ],
       declarations: [ EmployeeListComponent ]
     })
     .compileComponents();
@@ -17,14 +24,33 @@ describe('EmployeeListComponent', () => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    getEmployeesSpy = spyOn(employeeService, 'getEmployees').and.returnValue([{
+        id: 1,
+        name: 'Alex Dorogensky',
+        dept: 'System Engineering',
+        hired: new Date('3/18/2021'),
+        terminated: null
+      }, {
+        id: 2,
+        name: 'Nicole Mayo',
+        dept: 'Marketing',
+        hired: new Date('1/1/2015'),
+        terminated: null
+    }]);
   });
 
-  it('EmployeeListComponent is created', () => {
+  it('is created', () => {
     expect(component).toBeTruthy();
   });
 
-  it('EmployeeListComponent has an ag-grid-angular table', () => {
+  it('has an ag-grid-angular table', () => {
     const dom = fixture.nativeElement;
     expect(dom.querySelector('ag-grid-angular')).toBeTruthy();
+  });
+
+  it('has a "Name" table column', () => {
+    const dom = fixture.nativeElement;
+    expect(dom.querySelector('.ag-header').textContent).toContain('Name');
+    // TODO: more precise selector is not working, e.g. 'span[class = ag-header-cell-text][textContent = Name]'
   });
 });
