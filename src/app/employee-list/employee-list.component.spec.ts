cbb21e0d2a46bc232d577f07a29f3497be7e04bc
Alex Dorogensky <alex@exebar.com>
2021-09-12 18:32:26 -0700

    stubbed employee service, add tests for row data

--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { EmployeeListComponent } from './employee-list.component';
 import { EmployeeService } from '../employee.service';
 import { AgGridModule } from 'ag-grid-angular';
-import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator';
 
 describe('EmployeeListComponent', () => {
   let spectator: Spectator<EmployeeListComponent>;
@@ -9,31 +9,35 @@ describe('EmployeeListComponent', () => {
   let componentDom;
   const employeeService = new EmployeeService();
   let getEmployeesSpy;
+  const employee1Hired = '3/18/2021';
+  const employee2Hired = '1/1/2015';
+
   const stubbedEmployees = [{
     id: 1,
     name: 'Alex Dorogensky',
     dept: 'System Engineering',
-    hired: new Date('3/18/2021'),
+    hired: new Date(employee1Hired),
     terminated: null
   }, {
     id: 2,
     name: 'Nicole Mayo',
     dept: 'Marketing',
-    hired: new Date('1/1/2015'),
+    hired: new Date(employee2Hired),
     terminated: null
   }];
 
   const createComponent = createComponentFactory({
     component: EmployeeListComponent,
-    imports: [ AgGridModule ]
+    imports: [ AgGridModule ],
+    providers: [ mockProvider(EmployeeService, employeeService) ]
   });
 
   beforeEach(() => {
+    getEmployeesSpy = spyOn(employeeService, 'getEmployees').and.returnValue(stubbedEmployees);
     spectator = createComponent();
     component = spectator.fixture.componentInstance;
     componentDom = spectator.fixture.nativeElement;
     spectator.detectChanges();
-    getEmployeesSpy = spyOn(employeeService, 'getEmployees').and.returnValue(stubbedEmployees);
   });
 
   it('is created', () => {
@@ -73,5 +77,17 @@ describe('EmployeeListComponent', () => {
     expect(cellValues).toContainText(stubbedEmployees[0].dept);
     expect(cellValues).toContainText(stubbedEmployees[1].name);
     expect(cellValues).toContainText(stubbedEmployees[1].dept);
+
+    const rows = component.agGrid.rowData;
+    expect(rows).toHaveLength(2);
+    expect(rows[0]).toHaveAttribute('name', stubbedEmployees[0].name);
+    expect(rows[0]).toHaveAttribute('dept', stubbedEmployees[0].dept);
+    expect(rows[0]).toHaveAttribute('hired', employee1Hired);
+    expect(rows[0]).toHaveAttribute('terminated', null);
+
+    expect(rows[1]).toHaveAttribute('name', stubbedEmployees[1].name);
+    expect(rows[1]).toHaveAttribute('dept', stubbedEmployees[1].dept);
+    expect(rows[1]).toHaveAttribute('hired', employee2Hired);
+    expect(rows[1]).toHaveAttribute('terminated', null);
   });
 });
