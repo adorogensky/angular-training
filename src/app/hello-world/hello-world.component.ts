e65e44fb06b12bb2e02dd37c45562bea73fb508c
Alex Dorogensky <alex@exebar.com>
2022-06-10 15:16:59 -0700

    input change won't sync up with form group

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-hello-world',
@@ -8,10 +9,12 @@ import {Component, Input, OnInit} from '@angular/core';
 export class HelloWorldComponent implements OnInit {
 
   @Input() author: { name: string };
+  authFormGroup: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.authFormGroup = new FormBuilder().group(this.author);
   }
 
 }
