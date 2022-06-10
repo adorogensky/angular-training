385398103637b237bc7230c2765fe8fb2270764e
Alex Dorogensky <alex@exebar.com>
2022-06-10 15:07:41 -0700

    hello-world component now takes param that change initial value after 3s delay (imitatiting service call)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-training';
+  author = {
+    name: '[waiting on value...]'
+  };
+
+  ngOnInit(): void {
+    interval(3000).pipe().subscribe(() => {
+      this.author.name = 'Alex';
+    });
+  }
 }
