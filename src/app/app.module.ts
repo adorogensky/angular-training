316adc2ca70f9ba8ab417978b8b0bf07196f3f81
Alex Dorogensky <alex@exebar.com>
2022-07-11 19:37:49 -0700

    ng g c home/card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './home/toolbar/toolbar.component';
+import { CardComponent } from './home/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
